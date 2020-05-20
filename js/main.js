d4b0839fb53bd745c6746091d1fcbcfe0384309c
Bryan Tsai <twbikeman@gmail.com>
2020-05-20 16:01:58 +0800

    d3 start

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+d3.csv("data/ages.csv").then(function(data){
+    data.forEach(function(d){d.salary = +d.salary});
+    console.log(data);
+});
+
+
+
+
+
+
+
+// var data = [25, 20, 12, 15];
+// var svg = d3.select("#chart-area").append("svg").attr("width", 400).attr("height", 400)
+
+// var circles = svg.selectAll("circle").data(data);
+
+// circles.enter().append("circle").attr("cx", function(d, i){return (i * 50) + 25;}).attr("cy", 25).attr("r", function(d){return 25}).attr("fill", "red");
